2a6998ef4fc3c6362d0bb1d7445a7ad84fc0332e
Oriol Egea <egea@oriol.im>
2022-10-22 16:01:22 +0200

    Add a shared module to share modules between contexts

--- documents-service/src/index.ts
+++ documents-service/src/index.ts
@@ -1,18 +1,14 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
-import { startLoginController, validateLoginController } from './modules/login/infrastructure/controllers/factory'
+import { createDocumentController } from './modules/document/infrastructure/controllers/factory'
 
 dotenv.config({ path: '../.env' })
 const app = express()
 
-app.get('/login/:email', (req, res) => {
-  startLoginController(req, res).execute()
+app.post(':collection', (req, res) => {
+  createDocumentController(req, res).execute()
 })
 
-app.get('/login/:email/validate/:code', (req, res) => {
-  validateLoginController(req, res).execute()
-})
-
-app.listen(process.env.PASS_AUTH_PORT, () => {
-  console.log(`Authentication service is running on port ${process.env.PASS_AUTH_PORT}`)
+app.listen(process.env.DOCS_PORT, () => {
+  console.log(`Authentication service is running on port ${process.env.DOCS_PORT}`)
 })
