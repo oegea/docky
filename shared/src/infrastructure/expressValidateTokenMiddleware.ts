62f13632e8253e5c0e21e7b6ca38b67acdab32a1
Oriol Egea <egea@oriol.im>
2023-05-21 18:16:56 +0200

    Fix authentication bug

--- shared/src/infrastructure/expressValidateTokenMiddleware.ts
+++ shared/src/infrastructure/expressValidateTokenMiddleware.ts
@@ -4,7 +4,11 @@ export const expressValidateTokenMiddleware = (req, res, next) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (token == null) return res.sendStatus(401)
+  if (token == null) {
+    req.user = null
+    next()
+    return
+  }
 
   jwt.verify(token, process.env.COMMON_TOKEN_SECRET as string, (err: any, user: any) => {
 
