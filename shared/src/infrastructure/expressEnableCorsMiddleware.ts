b1ce051c0a7d048139b0baacd608cded16e75e5f
Oriol Egea <egea@oriol.im>
2023-06-04 18:01:25 +0200

    Add debugging logs to enable cors

--- shared/src/infrastructure/expressEnableCorsMiddleware.ts
+++ shared/src/infrastructure/expressEnableCorsMiddleware.ts
@@ -1,9 +1,16 @@
 import cors from 'cors'
 
 export const expressEnableCorsMiddleware = (req, res, next) => {
+    
     if (process.env.COMMON_DISABLE_CORS === 'false') {
+
+        (process.env.NODE_ENV === 'development') && console.log('CORS is enabled')
+
         cors()(req, res, next)
     } else {
+
+        (process.env.NODE_ENV === 'development') && console.log('CORS is disabled')
+
         next()
     }
 }
